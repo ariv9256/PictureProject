ab3a97b6d5873d0f8541ebc8c0719c7a12ff891a
Angela Rivas <snowcats001@gmail.com>
2019-01-23 13:25:46 -0700

    Added a revealPicture method in Picture.java for testSteganography

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -283,7 +283,23 @@ public class Picture extends SimplePicture
   }
   public void revealPicture()
   {
+	  Pixel[][] pixels = this.getPixels2D();
 	  
+	  for(int row = 0; row < pixels.length; row++)
+	  {
+		  for (int col = 0; col< pixels[0].length; col++)
+		  {
+			  //There will be a message to reveal 
+			  if (pixels[row][col].getRed() > 0 && pixels[row][col].getRed() % 2 != 1)
+			  {
+				  pixels[row][col].setColor(Color.CYAN);
+			  }
+			  else if(pixels[row][col].getRed() > 0 && pixels[row][col].getRed() % 2 ==1)
+			  {
+				  pixels[row][col].setColor(Color.MAGENTA);
+			  }
+		  }
+	  }
   }
   public void mirrorHorizontal()
   {
